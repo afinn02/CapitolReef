71e84cb0c2bc866228221ceb57e124a123711323
afinn02 <affinn@umcih.edu>
2024-01-25 14:49:32 -0500

    More file developments

--- R_start_sample_data.R
+++ R_start_sample_data.R
@@ -1,12 +1,20 @@
+#Preinstalls for script
 install.packages("readxl")
 install.packages("dplyr")
 install.packages("openxlsx")
+install.packages("RColorBrewer")
+install.packages('viridis')
 
+#Necessary libraries
 library(readxl)
 library(dplyr)
 library(openxlsx)
+library(RColorBrewer)
+library(viridis)
 
-file_path <- file.path("CapitolReef", "PhotoWildlifeData_UMichDataShare_12042023.xlsx")
+
+#file_path <- file.path("PhotoWildlifeData_UMichDataShare_12042023.xlsx")
+user_file_path <- readline(prompt = "Enter the file path: ")
 sheet_names <- excel_sheets(file_path)
 all_dataframes <- list()
 for (sheet in sheet_names) {
@@ -14,4 +22,22 @@ for (sheet in sheet_names) {
   all_dataframes[[sheet]] <- df
 }
 
+#Set up the different data frames from each of the main frames that we read in
+photoData <- all_dataframes[['PhotoData']]
+siteLocations <- all_dataframes[['SiteLocations']]
+metaData <- all_dataframes[['Metadata']]
 
+#Barplot of the different animals seen at each location:
+unique_species <- unique(photoData$Species1)
+num_colors <- length(unique_species)
+palette <- viridis(n = num_colors)
+species_color_vector <- palette[as.factor(photoData$Species1)]
+#species_color_vector <- species_colors[as.factor(photoData$Species1)]
+barplot(table(photoData$Species1),
+        xlab = "Species1",
+        ylab = "Frequency",
+        main = "Total Number of Species Observed",
+        col = species_color_vector)
+#legend('topright', legend = unique_species, fill = species_color_vector)
+
+       
